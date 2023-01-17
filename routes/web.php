d15cb822e267b600bb1559f89211b877c09dcf8e
crystalofsnow <tmiojpprog@gmail.com>
2023-01-17 23:37:08 +0900

    choose value --> subtotal

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/', [OrderController::class, 'index']);
 Route::get('/orders/{order}', [OrderController::class , 'choose']);
 //'Orders/{対象データID}'にGetリクエストが来たら，OrderControllerのchooseメソッドを実行する
 */
+Route::post('/orders/count',[OrderController::class, 'count']);
 Route::post('orders/merc',[OrderController::class, 'merc']);
 Route::post('/orders', [OrderController::class, 'send']);
 Route::get('/orders/choose', [OrderController::class, 'choose']);
